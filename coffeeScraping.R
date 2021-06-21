f6ba4634a45602662b7fc423e54227091da0a561
“Nancy <nancyorgan@gmail.com>
2021-06-21 14:48:57 -0700

    Processing script for other notes

--- coffeeScraping.R
+++ coffeeScraping.R
@@ -13,9 +13,12 @@ library("progress")
 library("ggplot2")
 library("d3r")
 library("jsonlite")
+library("Hmisc")
+library("reticulate")
+library("tidyr")
 
 ################################################################################
-############## Link click function #############################################
+################ Link click function #############################################
 
 beanbox = function(url){
     webpage <- read_html(url)
@@ -30,7 +33,7 @@ url = "https://beanbox.com/coffee/roast/blossom-coffee-roasters-dark-side-of-the
 beanbox(url)
 
 ################################################################################
-############# Details Function ##################################################
+################ Details Function ##################################################
 
 details = function(url){
   webpage <- read_html(url)
@@ -69,7 +72,7 @@ details = function(url){
 details(url)
 
 ################################################################################
-############# Get all Coffees ##################################################
+################ Get all Coffees ##################################################
 
 getAllCoffees = function(max){
   total <- max
@@ -162,7 +165,7 @@ allCoffees2 =
 #   geom_point()
 
 ################################################################################
-################# Scrape Lexicon ################################################
+################# Scrape Lexicon ###############################################
 url = "https://onlinelibrary.wiley.com/doi/full/10.1111/1750-3841.13555"
 
 lexicon = function(url){
@@ -197,21 +200,56 @@ flavors = lexicon("https://onlinelibrary.wiley.com/doi/full/10.1111/1750-3841.13
 flavors$col1[flavors$col1 == ""] = NA
 flavors$col2[flavors$col2 == ""] = NA
 flavors$col3[flavors$col3 == ""] = NA
+# flavors$col3[!is.na(flavors$col1)] = 1
 
 flavorsFilled = tibble(flavors) %>%
   fill(col1) %>%
-  fill(col2) 
-flavorsFilled 
+  fill(col2)
 
-names(flavorsFilled)[names(flavorsFilled) == "col3"] = "name"
-out = split(flavorsFilled, flavorsFilled$col1) 
-out = lapply(out, function(x){ return(x[,c(2,3)])})
-out = lapply(out, function(x){ return(split(x, x$col2))})
-for(i in 1:length(out)){
-  out[[i]] = lapply(out[[i]], function(x){ return(x[,c(2)])})
-}
+flavorsFilled = cbind("flavors", flavorsFilled)
+# flavorsFilled$value = 1
 
-prettify(toJSON(out))
-toJSON(flavors)
+# names(flavorsFilled)[names(flavorsFilled) == "col3"] = "name"
+# out = split(flavorsFilled, flavorsFilled$col1)
+# out = lapply(out, function(x){ return(x[,c(2,3)])})
+# out = lapply(out, function(x){ return(split(x, x$col2))})
+# for(i in 1:length(out)){
+#   out[[i]] = lapply(out[[i]], function(x){ return(x[,c(2)])})
+# }
+# 
+prettify(toJSON(flavorsFilled))
 
-split()
+toJSON(flavorsFilled, pretty = TRUE)
+write.csv(flavorsFilled, "/Users/nancyorgan/Documents/Coffee/flavors.csv", row.names = FALSE)
+################################################################################
+################# Get Unique Notes #############################################
+scrapedFlavors = unique(allCoffees$taste)
+scrapedFlavors
+
+lexiconFlavors = unique(c(flavors$col1, flavors$col2, flavors$col3))
+sort(lexiconFlavors)
+
+sort(scrapedFlavors[scrapedFlavors %nin% lexiconFlavors])
+
+#  [1] "Apricot"           "Banana"            "Bergamot"          "Black Cherry"     
+#  [5] "Black Currant"     "Black Tea"         "Brown Sugar"       "Brownie"          
+#  [9] "Butterscotch"      "Cacao"             "Caramel"           "Cedar"            
+# [13] "Champagne"         "Chestnut"          "Citrus"            "Cola"             
+# [17] "Cookie"            "Cookie Dough"      "Cranberry"         "Cream"            
+# [21] "Currant"           "Dark Chocolate"    "Date"              "Dried Fruit"      
+# [25] "Fig"               "Fruit"             "Fudge"             "Ginger"           
+# [29] "Graham Cracker"    "Guava"             "Herbal"            "Honeydew"         
+# [33] "Huckleberry"       "Kiwi"              "Kumquat"           "Lavender"         
+# [37] "Licorice"          "Mango"             "Maple"             "Maple Syrup"      
+# [41] "Marionberry"       "Marmalade"         "Marshmallow"       "Marzipan"         
+# [45] "Melon"             "Meyer Lemon"       "Milk Chocolate"    "Mint"             
+# [49] "Mountain Apple"    "Nectarine"         "Nougat"            "Oak"              
+# [53] "Orange Blossom"    "Papaya"            "Passion Fruit"     "Pecan"            
+# [57] "Plum"              "Praline"           "Red Grape"         "Red Wine"         
+# [61] "Rhubarb"           "Roasted Chestnuts" "Sage"              "Shortbread"       
+# [65] "Spice"             "Stone Fruit"       "Tamarind"          "Toast"            
+# [69] "Toffee"            "Tomato"            "Tropical"          "Tropical Fruit"   
+# [73] "Walnuts"           "Watermelon"        "Wine"             
+
+write.csv(flavorsFilled, "/Users/nancyorgan/Documents/Coffee/flavors.csv", row.names = FALSE)
+
